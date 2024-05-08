a4b026d22b721e1d57bfae02e0b01664f0d6e401
soumik890 <soumik890@gmail.com>
2024-05-09 00:23:19 +0530

    fifth

--- fifth.go
+++ fifth.go
@@ -7,12 +7,24 @@ import (
 func main() {
 	fmt.Println("fifth file")
 	password := "12345678"
-
 	if len(password) > 7 {
 		fmt.Println("The length is greater than 7")
 	} else if len(password) > 8 {
 		fmt.Println("Its length is greater than 8")
 	} else {
 		fmt.Println("The length is not greater than 7 ")
+	}
+
+	for i := 0; i <= 5; i++ {
+		if i == 3 {
+			continue
+		}
+		fmt.Println(i)
+	}
+	for i := 0; i <= 5; i++ {
+		if i == 3 {
+			break
+		}
+		fmt.Println(i)
 	}
 }
