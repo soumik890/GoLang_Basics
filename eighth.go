fc731ab6b7d92928501efba9b84fabadf97c6b43
soumik890 <soumik890@gmail.com>
2024-05-13 11:15:27 +0530

    eight

--- eighth.go
+++ eighth.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-
 	fmt.Println("Eighth File")
+	slice := make([]string, 3, 5)
+	fmt.Println(slice)
+	fmt.Println("Length", len(slice))
+	fmt.Println("Capacity", cap(slice))
+
+	num := []int{10, 20, 30, 40}
+	fmt.Println(num)
+	fmt.Println(num[:])
+	fmt.Println(num[0:2])
+
+	num = append(num, 50, 60, 70, 80)
+	fmt.Println(num)
 }
